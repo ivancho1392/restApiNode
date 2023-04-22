2c2087656fa91f3239cf23acab14ad28e0cae7fb
ivancho1392 <ivancho1392@gmail.com>
2023-04-21 20:29:03 -0500

    add localhost:3000 to whitelist

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:3000'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
