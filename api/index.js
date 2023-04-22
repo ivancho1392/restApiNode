4fbd0495c0aa3138433845ab96ec293210b2dca0
ivancho1392 <ivancho1392@gmail.com>
2023-04-21 20:43:21 -0500

    change cors options for test API

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:3000'];
+/*const whitelist = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:3000'];
 const options = {
   origin: (origin, callback) => {
     if (whitelist.includes(origin) || !origin) {
@@ -22,7 +22,12 @@ const options = {
       callback(new Error('no permitido'));
     }
   }
-}
+}*/
+
+const options = {
+  origin: '*',
+};
+
 app.use(cors(options));
 
 app.get('/api', (req, res) => {
